eda09a1fa5265e1af81a3408596755990a7811c4
00Whitehacker00 <kasundi.walgama@gmail.com>
2023-04-25 11:53:46 +0300

    Commit 5

--- app/src/main/java/java/main/lutemonfighter/Lutemon.java
+++ app/src/main/java/java/main/lutemonfighter/Lutemon.java
@@ -6,6 +6,12 @@ public class Lutemon implements Serializable {
 
     private String name;
     private String lutemonType;
+    protected int attack;
+    protected int defend;
+    protected int experience;
+    protected int health;
+    protected int maxHealth;
+    protected int id;
 
     public Lutemon(String name, String lutemonType) {
         this.name = name;
